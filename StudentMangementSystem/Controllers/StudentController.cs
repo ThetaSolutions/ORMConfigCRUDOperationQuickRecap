e96474c560c67fed69dc04bd85e7fdae81a7b958
g4genious <g_4_genious@yahoo.com>
2019-05-11 12:27:27 +0500

    CURD operations done in controller.

---
+++ StudentMangementSystem/Controllers/StudentController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StudentMangementSystem.Models;
+
+namespace StudentMangementSystem.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly StudentManagementSystemContext ORM = null;
+
+        public StudentController(StudentManagementSystemContext _ORM)
+        {
+            ORM = _ORM;
+        }
+
+        [HttpGet]
+        public IActionResult AddNewStudent()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult AddNewStudent(Student S)
+        {
+            ORM.Student.Add(S);
+            ORM.SaveChanges();
+
+            return RedirectToAction(nameof(StudentController.ShowAllStudents));
+        }
+
+        public IActionResult ShowAllStudents()
+        {
+           return View(ORM.Student.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult EditStudent(int id)
+        {
+            return View(ORM.Student.Find(id));
+        }
+
+        [HttpPost]
+        public IActionResult EditStudent(Student S)
+        {
+            ORM.Student.Update(S);
+            ORM.SaveChanges();
+
+            return RedirectToAction(nameof(StudentController.ShowAllStudents));
+        }
+
+        public IActionResult DeleteStudent(int id)
+        {
+            ORM.Student.Remove(ORM.Student.Find(id));
+            ORM.SaveChanges();
+            return RedirectToAction(nameof(StudentController.ShowAllStudents));
+
+
+
+        }
+
+
+
+    }
+}
